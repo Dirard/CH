5d3dd8a3e4942b4848dba5e37ef300f156b91a95
Dirard <ardagin_aa@mail.ru>
2019-01-17 14:47:14 +0300

    secret

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ClanWeb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     config.assets.paths << Rails.root.join("app", "assets", "font", "roboto")
     config.assets.paths << Rails.root.join("app", "assets", "font")
     config.assets.paths << Rails.root.join("vendor", "assets", "img")
